35a35edb16849b604dadc31c17758d50372d5cfd
Matematyka.KR <Matematyka.KR@gmail.com>
2012-01-22 09:21:48 +0000

    basic untestes code for agents

---
+++ SocialNet/src/social/model/FriendInfo.java
@@ -0,0 +1,60 @@
+package social.model;
+
+import java.io.Serializable;
+
+import jade.core.AID;
+
+public class FriendInfo implements Serializable{
+
+	private static final long serialVersionUID = -6399463456418609220L;
+	
+	
+	String ideaId;
+	String id;
+	Integer ideaSurence;
+	AID parentAID;
+	Integer infuence;
+	
+	public FriendInfo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public FriendInfo(Person person) {
+
+		ideaId = person.getIdeaId();
+		id = person.getId();
+		ideaSurence = person.getIdeaSurance();
+		parentAID = person.getParentAID();
+	
+	}	
+	public String getIdeaId() {
+		return ideaId;
+	}
+	public void setIdeaId(String ideaId) {
+		this.ideaId = ideaId;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Integer getIdeaSurence() {
+		return ideaSurence;
+	}
+	public void setIdeaSurence(Integer ideaSurence) {
+		this.ideaSurence = ideaSurence;
+	}
+	public AID getParentAID() {
+		return parentAID;
+	}
+	public void setParentAID(AID parentAID) {
+		this.parentAID = parentAID;
+	}
+	public Integer getInfuence() {
+		return infuence;
+	}
+	public void setInfuence(Integer infuence) {
+		this.infuence = infuence;
+	}
+}
--- SocialNet/src/social/model/Person.java
+++ SocialNet/src/social/model/Person.java
@@ -1,30 +1,175 @@
 package social.model;
 
+import jade.core.AID;
+
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class Person {
 
-	String name;
-	Idea idea;
-	List<Person> friends;
+	static Random random = new Random(System.nanoTime());
+
+	String id;
+
+	String ideaId;
+
+	Integer ideaSurance;
+
+	Integer latituide, logitiude;
 	
-	public String getName() {
-		return name;
+	Map<String, FriendInfo> friends = new HashMap<String, FriendInfo>();
+
+	AID parentAID;
+
+	public String recalculateIdea() {
+		return recalculateIdea(this.ideaId, 0);
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public String recalculateIdea(String ideaId, Integer infuance) {
+
+		Map<String, Integer> ideas = new HashMap<String, Integer>();
+
+		ideas.put(ideaId, infuance);
+
+		for (FriendInfo friendInfo : friends.values()) {
+			Integer tmp = ideas.containsKey(friendInfo.getIdeaId()) ? 0 : ideas
+					.get(friendInfo.ideaId);
+			tmp += friendInfo.getIdeaSurence() / friendInfo.getInfuence();
+			ideas.put(friendInfo.getIdeaId(), tmp);
+		}
+
+		String newIdea = ideaId;
+		Integer newSurance = infuance;
+
+		for (String idea : ideas.keySet()) {
+
+			if (ideas.get(idea) > infuance) {
+				infuance = ideas.get(idea);
+				newIdea = idea;
+			}
+
+		}
+
+		this.ideaSurance = newSurance;
+
+		if (newIdea == this.ideaId) {
+			return null;
+		}
+
+		this.ideaId = newIdea;
+
+		return newIdea;
 	}
-	public Idea getIdea() {
-		return idea;
+
+	public boolean newFriendRequest(FriendInfo info) {
+
+		if (!friends.containsKey(info.getId()) && random.nextBoolean()) {
+			info.setInfuence(random.nextInt(StaticValues.maxInfuence));
+			friends.put(info.getId(), info);
+			return true;
+		}
+		return false;
 	}
-	public void setIdea(Idea idea) {
-		this.idea = idea;
+
+	
+	public String statusChange(String friendID, String ideaID, int surance){
+		
+		FriendInfo info = friends.get(friendID);
+		if(info != null){
+			
+			info.setIdeaId(ideaID);
+			info.setIdeaSurence(surance);
+			info.setInfuence(random.nextInt(StaticValues.maxInfuence));
+			
+			recalculateIdea();
+		}
+		
+		
+		return null;
 	}
-	public List<Person> getFriends() {
+	
+	public void removeFried(String personID){
+		friends.remove(personID);
+	}
+	
+	public FriendInfo removeFriend() {
+		String toDel = null;
+
+		if (random.nextBoolean()) {
+			for (String id : friends.keySet()) {
+				if (random.nextInt() == 3) {
+					toDel = id;
+					break;
+				}
+			}
+		}
+
+		if (toDel != null) {
+			return friends.remove(toDel);
+		}
+		return null;
+	}
+	
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getIdeaId() {
+		return ideaId;
+	}
+
+	public void setIdeaId(String ideaId) {
+		this.ideaId = ideaId;
+	}
+
+	public Integer getIdeaSurance() {
+		return ideaSurance;
+	}
+
+	public void setIdeaSurance(Integer ideaSurance) {
+		this.ideaSurance = ideaSurance;
+	}
+
+	public Integer getLatituide() {
+		return latituide;
+	}
+
+	public void setLatituide(Integer latituide) {
+		this.latituide = latituide;
+	}
+
+	public Integer getLogitiude() {
+		return logitiude;
+	}
+
+	public void setLogitiude(Integer logitiude) {
+		this.logitiude = logitiude;
+	}
+
+	public Map<String, FriendInfo> getFriends() {
 		return friends;
 	}
-	public void setFriends(List<Person> friends) {
+
+	public void setFriends(Map<String, FriendInfo> friends) {
 		this.friends = friends;
 	}
-	
+
+	public AID getParentAID() {
+		return parentAID;
+	}
+
+	public void setParentAID(AID parentAID) {
+		this.parentAID = parentAID;
+	}
+
+
+
 }
