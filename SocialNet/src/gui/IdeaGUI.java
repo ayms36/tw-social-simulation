80f8bda4bffa386b13ada46851a475f4d3a09ca3
jgwozdz1 <jgwozdz1@gmail.com>
2012-01-25 07:17:33 +0000

    Naprawiłem kolorki.  I zrobilem ze na koniec rysuje kolka zeby lepiej wygladalo jak cos jeszcze to pisz Noi bardziej na srodku zrobilem aby rysowalo

--- SocialNet/src/gui/IdeaGUI.java
+++ SocialNet/src/gui/IdeaGUI.java
@@ -4,12 +4,8 @@ import java.awt.Color;
 import java.util.Random;
 
 public class IdeaGUI {
-	String name;
 	Color color;
-	public IdeaGUI(String name) {
-		if(name == null)
-			throw new NullPointerException("name cannot be null");
-		this.name = name;
+	public IdeaGUI() {
 		Random r = new Random();
 		this.color = new Color(r.nextInt(255),r.nextInt(255),r.nextInt(255));
 	}
@@ -18,22 +14,5 @@ public class IdeaGUI {
 	}
 	public void setC(Color c) {
 		this.color = c;
-	}
-	public String getName() {
-		return name;
-	};
-	
-	@Override
-	public boolean equals(Object a) {
-		if(a != null && a instanceof IdeaGUI){
-			return name.equals(((IdeaGUI) a).getName());
-		}
-		return false;
-	}
-
-	
-	
-	public String toString(){
-		return name;
 	}
 }
